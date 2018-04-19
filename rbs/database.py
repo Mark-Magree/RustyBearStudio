3e86aef535687de4ebdf253d062ae61701343e81
Mark Magree <markmagree@protonmail.com>
2018-04-19 18:31:37 -0400

    modify_database cleanups and DB table changes. honestly can't remember, it's been a few days

--- rbs/database.py
+++ rbs/database.py
@@ -9,14 +9,23 @@ class Event(Base):
     __tablename__ = 'event'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    venue = Column(String(250))
+    days = Column(Integer)
+    time_start = Column(String(32))
+    time_end = Column(String(32))
     day = Column(String(32))
     month = Column(String(32))
+    time_start2 = Column(String(32))
+    time_end2 = Column(String(32))
+    day2 = Column(String(32))
+    month2 = Column(String(32))
     year = Column(String(32))
     street_name = Column(String(250))
     street_number = Column(String(250))
     city = Column(String(250))
+    details = Column(String(250))
     def __repr__(self):
-        return f"<Event(name={self.name}, date={self.day}/{self.month}/{self.year}, address={self.street_number} {self.street_name}, {self.city})>"
+        return f"<Event(name={self.name}, days={self.days}, date={self.day}/{self.month}/{self.year}, address={self.street_number} {self.street_name}, {self.city})>"
 
 class Image(Base):
     __tablename__ = 'image'
--- rbs/modify_database.py
+++ rbs/modify_database.py
@@ -2,9 +2,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from database import Event, Image, Base
+from datetime import datetime, timedelta
 from os import listdir
 from os.path import isdir
 import subprocess
+
+IGNORED_DIRS = ['deco_images',]
 
 def add_events(s):
     def commit_or_not(event):
@@ -25,15 +28,45 @@ def add_events(s):
         name = input("name (leave blank if finished): ")
         if name == '':
             break
-        year = input("year (2 digit): ")
-        month = input("month (2 digit): ")
-        day = input("date (2 digit): ")
-        street_name = input("street name: ")
+        venue = input("venue: ")
+        year = input("year (4 digit): ")
+        month = input("month (2 digit): ").zfill(2)
+        day = input("date (2 digit): ").zfill(2)
+        time_start = input("time to start (formatted, like '10:00am'): ")
+        time_end = input("time to end (formatted, like '4:00 pm'): ")
+        is_two_days = input("Is this a two day show? (y/N): ")
+        if len(is_two_days) == 0:
+            is_two_days = "n"
+        if is_two_days[0].lower() == "y":
+            days = 2
+            show_date = datetime.strptime(f"{day}-{month}-{year}", "%d-%m-%Y")
+            show_date2 = show_date + timedelta(days=1)
+            month2 = str(show_date2.month).zfill(2)
+            day2 = str(show_date2.day).zfill(2)
+            time_start2 = input("time to start 2nd day (formatted, like '10:00am'): ")
+            time_end2 = input("time to end 2nd day (formatted, like '4:00 pm'): ")
+        else:
+            days = 1
+            month2 = "none"
+            day2 = "none"
+            time_start2 = "none"
+            time_end2 = "none"
         street_number = input("street number: ")
+        street_name = input("street name: ")
         city = input("city: ")
-        new_event = Event(name=name,year=year,month=month,day=day,
+        details = input("details (parking, entrance fee, we will be outdoors/indoors...): ")
+        new_event = Event(name=name, venue=venue, days=days, 
+                            year=year,
+                            month=month, day=day,
+                            time_start=time_start, time_end=time_end,
+                            month2=month2, day2=day2,
+                            time_start2=time_start2, time_end2=time_end2,
                             street_name=street_name,
-                            street_number=street_number,city=city)
+                            street_number=street_number,
+                            city=city,
+                            details=details)
+        commit_or_not(new_event)
+        print()
 
 
 def add_new_images():
@@ -51,7 +84,7 @@ def add_new_images():
             print("Image not saved to database. Run again to add.")
         else:
             commit_or_not(image)
-    dir_list = [x for x in listdir('static') if isdir(f'static/{x}')]
+    dir_list = [x for x in listdir('static') if isdir(f'static/{x}') and not x in IGNORED_DIRS]
     print("directoyr list:", dir_list)
     for d in dir_list:
         for i in listdir(f"static/{d}"):
